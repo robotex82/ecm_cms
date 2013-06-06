c249aeb0c97c80b7fff8a7f6d725377232e99bb3
Roberto Vasquez Angel <robotex@robotex.de>
2013-06-06 22:18:55 +0200

    Fixed: Page Controller did not include the partial resolver.

--- app/controllers/ecm/cms/page_controller.rb
+++ app/controllers/ecm/cms/page_controller.rb
@@ -1,14 +1,20 @@
 class Ecm::Cms::PageController < ApplicationController
   # inject locale to params and vice versa
   include RailsTools::I18nController
-  
+
   # add the page resolver
   include Ecm::Cms::ControllerExtensions::PageResolver
+  include Ecm::Cms::ControllerExtensions::PartialResolver
 
   # avoid error 500 on missing template
   rescue_from ActionView::MissingTemplate do
     respond_to do |format|
-      format.html { render(:file => "#{Rails.root}/public/404", :formats => [:html], :layout => false, :status => 404) }
+      format.html {
+        render(:file => "#{Rails.root}/public/404", :formats => [:html],
+                                                    :layout => false,
+                                                    :status => 404
+        )
+      }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
@@ -18,3 +24,4 @@ class Ecm::Cms::PageController < ApplicationController
     render :template => params[:page]
   end
 end
+
