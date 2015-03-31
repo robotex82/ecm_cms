2928ba3fd146d978cdffc395e440dc4d5e6f2d10
Roberto Vasquez Angel <robotex@robotex.de>
2015-04-01 00:45:06 +0200

    Enhancement: Added rails 4.x support.

--- lib/ecm/cms/action_view/template_renderer_patch.rb
+++ lib/ecm/cms/action_view/template_renderer_patch.rb
@@ -3,23 +3,39 @@ module ActionView
     def self.included base
       base.class_eval do
         alias_method :original_render, :render
+        if Rails::VERSION::MAJOR < 4
+          def render(context, options)
+            @view = context
+            @details = extract_details(options)
+            extract_format(options[:file] || options[:template], @details)
+            template = determine_template(options)
+            context = @lookup_context
 
-        def render(context, options)
-          @view = context
-          @details = extract_details(options)
-          extract_format(options[:file] || options[:template], @details)
-          template = determine_template(options)
-          context = @lookup_context
+            unless context.rendered_format
+              context.formats = template.formats unless template.formats.empty?
+              context.rendered_format = context.formats.first
+            end
 
-          unless context.rendered_format
-            context.formats = template.formats unless template.formats.empty?
-            context.rendered_format = context.formats.first
+            layout = template.layout if template.respond_to?(:layout)
+            layout ||= options[:layout]
+
+            render_template(template, layout, options[:locals])
           end
+        elsif Rails::VERSION::MAJOR < 5
+          def render(context, options)
+            @view = context
+            @details = extract_details(options)
+            template = determine_template(options)
+            
+            prepend_formats(template.formats)
+            
+            @lookup_context.rendered_format ||= (template.formats.first || formats.first)
 
-          layout = template.layout if template.respond_to?(:layout)
-          layout ||= options[:layout]
-
-          render_template(template, layout, options[:locals])
+            layout = template.layout if template.respond_to?(:layout)
+            layout ||= options[:layout]
+            
+            render_template(template, layout, options[:locals])
+          end
         end
       end
     end
