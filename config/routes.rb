96c9e2007171a3c57c0dab734964d0fc847fdd77
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-10-30 10:06:32 +0100

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,2 @@
 Ecm::Cms::Engine.routes.draw do
-#  if Rails.version < '4.0.0'
-#    get "/*page", :to => "ecm/cms/page#respond", :as => :page
-#  else
-#    localized do
-#      get "/*page", :to => "ecm/cms/page#respond", :as => :page
-#      # get "/:page", :to => "ecm/cms/page#respond", page: /.*/, :as => :page
-#    end
-#  end
-
-##  root to: "ecm/cms/page#respond", :page => 'home' 
-#  # get '/' => "ecm/cms/page#respond", :page => 'home', :as => :'main_app.root'
 end
